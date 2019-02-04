10b28d701e2b3fa57c914e76d00023cf2ed118c2
Jeremy Lee <jlee00@stuy.edu>
2019-02-03 21:23:53 -0500

    Added untested sqrt function

--- Recursion.java
+++ Recursion.java
@@ -1,4 +1,4 @@
-public class recursion{
+public class Recursion{
     /*You may write additional private methods */
 
     /*Recursively find the sqrt using Newton's approximation
@@ -7,9 +7,23 @@ public class recursion{
 
     */
     public static double sqrt(double n, double tolerance){
-
+      if (n < 0){
+        throw new IllegalArgumentException("Negatives can't have a square root.");
+      }
+      if (n == 0){
+        return 0;
+      }
+      double guess = n/2;
+      return sqrtHelper(n, guess, tolerance);
     }
 
+    public static double sqrtHelper(double n, double guess, double tolerance){
+      if ((n/(guess*guess) >= (1-tolerance/100)) && (n/(guess*guess) <= (1+tolerance/100))){
+        return guess;
+      } else {
+        return sqrtHelper(n, (n/guess + guess)/2, tolerance);
+      }
+    }
     /*Recursively find the n'th fibbonaci number in linear time
      *fib(0) = 0
      *fib(1) = 1
@@ -17,11 +31,16 @@ public class recursion{
      *precondition: n is non-negative
      */
     public static int fib(int n){
-
+      return 0;
     }
 
     /*As Per classwork*/
-    public static ArrayList<Integer> makeAllSums(int n){
+    // public static ArrayList<Integer> makeAllSums(int n){
+    //
+    // }
+
+    public static void main(String[] args){
+
     }
 
 }
